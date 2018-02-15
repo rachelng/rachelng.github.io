f40053871992252140039bdba31cfe4f5f158c9a
Rachel <rachel@rachel-ng.com>
2018-02-14 21:05:53 -0500

    completed homework

---
+++ homework/99_bottles_of_beer.js
@@ -0,0 +1,12 @@
+// 99 BOTTLES OF BEER
+// Write a script that prints the lyrics to "99 Bottles of Beer on the Wall" in the terminal
+// Make sure your program can handle both singular and plural cases (i.e. both "99 bottles of beer" and "1 bottle of beer")
+// Hint: you will want to use a for loop that starts at 99 and counts down to 0
+
+for (var i = 0; i <= 99; i++) {
+	if (i === 1) {
+		console.log(i + ' bottle of beer on the wall');
+	} else {
+		console.log(i + ' bottles of beer on the wall');
+	}
+}
